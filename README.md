f33abdd48127b58b15708849dff30ee08258a44b
Greg-Bush <greg.bush.main@icloud.com>
2019-04-11 21:49:27 +0300

    react-native-dimensions-hoc

---
+++ README.md
@@ -0,0 +1,41 @@
+# react-native-dimensions-hoc
+
+> A HOC to provide width and height properties with actual dimensions for your React-Native components.
+
+## Basic Usage
+
+```tsx
+import React from 'react';
+import { ViewProps } from "react-native";
+import { withDimensions, IDimensionsProps } from "react-native-dimensions-hoc";
+
+export interface YourComponentProps extends ViewProps {
+    ...
+}
+
+class YourComponent extends React.Component<YourComponentProps & IDimensionsProps> {
+
+    render() {
+        const { width, height, remeasure, ...viewProps } = this.props;
+        return (
+            <View {...viewProps} onLayout={this.props.remeasure}>
+                ...
+            </View>
+        );
+    }
+    ...
+}
+
+export default withDimensions<YourComponentProps>(YourComponent);
+```
+
+
+## Installation
+
+```
+$ npm install react-native-dimensions-hoc --save
+```
+
+## License
+
+MIT
---
+++ index.tsx
@@ -0,0 +1,55 @@
+import hoistNonReactStatics from 'hoist-non-react-statics';
+import * as React from 'react';
+import { LayoutChangeEvent, View, ViewProps } from 'react-native';
+
+export interface IDimensionsProps extends ViewProps {
+	width: number;
+	height: number;
+	remeasure: (e: LayoutChangeEvent) => void;
+}
+
+interface IForwardedRefProp<T = any> {
+	forwardedRef: React.Ref<T>;
+}
+/**
+ * @description A HOC to provide width and height properties with actual dimensions for your React-Native components.
+ * @author https://github.com/Greg-Bush
+ * @date 2019-04-11
+ * @template TResultProps
+ * @param {React.ComponentType<IDimensionsProps>} WrappedComponent
+ */
+export default function withDimensions<TResultProps extends ViewProps>(
+	WrappedComponent: React.ComponentType<IDimensionsProps>,
+) {
+	class WithDimensions extends React.Component<TResultProps & IForwardedRefProp<typeof WrappedComponent>> {
+		public static displayName = `withDimensions(${getDisplayName(WrappedComponent)})`;
+		public state = { width: 0, height: 0, initialRender: false };
+		public render() {
+			const { forwardedRef, ...props } = this.props;
+			return (
+				this.state.initialRender ?
+					(
+						<WrappedComponent
+							ref={forwardedRef}
+							{...props}
+							width={this.state.width}
+							height={this.state.height}
+							remeasure={this.measure}
+						>
+							{this.props.children}
+						</WrappedComponent>
+					) :
+					<View style={this.props.style || { flex: 1 }} onLayout={this.measure} />
+			);
+		}
+		private measure = ({ nativeEvent: { layout: { width = 0, height = 0 } = {} } = {} }) => {
+			this.setState({ width, height, initialRender: true });
+		}
+	}
+	hoistNonReactStatics(WithDimensions, WrappedComponent);
+	return React.forwardRef<typeof WrappedComponent, TResultProps>((props, ref) =>
+		(<WithDimensions {...props} forwardedRef={ref}>{props.children}</WithDimensions>));
+}
+
+const getDisplayName = (WrappedComponent: React.ComponentType<any>) =>
+	WrappedComponent.displayName || WrappedComponent.name || 'WrappedComponent';
